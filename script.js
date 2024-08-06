e180446c4084d6f6b4b1f91a04a11100d13a0ec3
nandini426 <218x1a0426@khitguntur.ac.in>
2024-08-06 15:02:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('JavaScript loaded.');
+    // You can add interactivity here, such as event listeners or dynamic content updates
+});
